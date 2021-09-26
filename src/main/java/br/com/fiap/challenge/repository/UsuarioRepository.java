b725ea92c8d370e321e6914c7d32704bab50a3da
PedroUtida <61791024+PedroUtida@users.noreply.github.com>
2021-09-26 16:49:45 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/fiap/challenge/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/challenge/repository/UsuarioRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UsuarioRepository extends JpaRepository<Atleta, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     List<Usuario> findByNomeContainsIgnoreCase(String nome);
 
